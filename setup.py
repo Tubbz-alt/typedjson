a10fd93d093ed94e11e951384a2cc27969ba0c33
Murali Shankar <mshankar@slac.stanford.edu>
2020-04-08 14:30:35 -0700

    Initial copy from https://raw.githubusercontent.com/slac-lcls/lcls2/master/psalg/psalg/configdb/typed_json.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='typed_json',
+      version='0.0.1',
+      description='The typed_json for LCLS2/PCDS configdb',
+      url='https://github.com/slac-lcls/typedjson',
+      author='Michael Browne',
+      author_email='mcbrowne@slac.stanford.edu',
+      license='Apache 2',
+      packages=['typed_json'],
+      zip_safe=False)
